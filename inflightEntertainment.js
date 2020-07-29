5878e9fcbe9125f668396e652269b493e2407bf6
Kristine Du <kristine.a.du@gmail.com>
2020-07-29 12:32:03 -0600

    Update solution with Set.

--- inflightEntertainment.js
+++ inflightEntertainment.js
@@ -1,16 +1,33 @@
-module.exports = function watchMovies(flightTime, movieLengths) {
-    let movies = {}
+// module.exports = function watchMovies(flightTime, movieLengths) {
+//     let movies = {}
 
-    movieLengths.forEach(movie => movies[movie] ? movies[movie] += 1 : movies[movie] = 1)
+//     movieLengths.forEach(movie => movies[movie] ? movies[movie] += 1 : movies[movie] = 1)
+
+//     for(let i = 0; i < movieLengths.length; i++){
+//         let firstMovie = movieLengths[i]
+//         let secondMovie = flightTime - firstMovie
+//         movies[firstMovie] -= 1
+
+//         if (movies[secondMovie] && movies[secondMovie] > 0) {
+//             return true
+//         }
+//     }
+
+//     return false
+// }
+
+module.exports = function watchMovies(flightTime, movieLengths) {
+    let movies = new Set()
 
     for(let i = 0; i < movieLengths.length; i++){
         let firstMovie = movieLengths[i]
         let secondMovie = flightTime - firstMovie
-        movies[firstMovie] -= 1
 
-        if (movies[secondMovie] && movies[secondMovie] > 0) {
+        if (movies.has(secondMovie)){
             return true
         }
+        
+        movies.add(firstMovie)
     }
 
     return false
